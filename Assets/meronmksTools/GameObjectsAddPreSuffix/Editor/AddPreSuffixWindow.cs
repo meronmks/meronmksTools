b9b2864bce3003a6d7d66091a3ab7f17f675d703
meronmks <meronmks@users.noreply.github.com>
2021-08-28 17:09:15 +0900

    最初のコミット

---
+++ Assets/meronmksTools/GameObjectsAddPreSuffix/Editor/AddPreSuffixWindow.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MeronmksTools
+{
+    public class AddPreSuffixWindow : EditorWindow
+    {
+        [SerializeField] private Transform g_RootGameObject;
+        private String prefixText;
+        private String suffixText;
+        private bool isRootObjectRename = true;
+        private bool isDisabled = true;
+        
+        [MenuItem("Tools/AddPreSuffix")]
+        static void ShowWindow()
+        {
+            var window = EditorWindow.GetWindow<AddPreSuffixWindow>();
+            window.titleContent.text = "AddPreSuffix";
+            window.Show();
+        }
+    
+        private void ChangeAllUnderObjectName(Transform transform)
+        {
+            foreach (Transform childTransform in transform)
+            {
+                Undo.RecordObject(childTransform.gameObject, $"AddPreSuffix:{g_RootGameObject.name}");
+                childTransform.name = prefixText + childTransform.name + suffixText;
+                ChangeAllUnderObjectName(childTransform);
+            }
+        }
+        private void OnGUI()
+        {
+            g_RootGameObject = (Transform) EditorGUILayout.ObjectField("RootGameObject", g_RootGameObject, typeof(Transform), true);
+            prefixText = EditorGUILayout.TextField("Prefix", prefixText);
+            suffixText = EditorGUILayout.TextField("Suffix", suffixText);
+            isRootObjectRename = EditorGUILayout.Toggle("Rootオブジェクトもリネーム対象とする", isRootObjectRename);
+            if (g_RootGameObject == null)
+            {
+                EditorGUILayout.HelpBox("RootGameObjectが未設定です", MessageType.Error);
+                isDisabled = true;
+            }
+            else
+            {
+                isDisabled = false;
+            }
+    
+            if (String.IsNullOrWhiteSpace(prefixText) && String.IsNullOrWhiteSpace(suffixText))
+            {
+                EditorGUILayout.HelpBox("PrefixおよびSuffixが未設定です", MessageType.Warning);
+            }
+            using (new EditorGUI.DisabledScope(isDisabled))
+            {
+                if (GUILayout.Button("実行"))
+                {
+                    if (isRootObjectRename)
+                    {
+                        Undo.RecordObject(g_RootGameObject.gameObject, $"AddPreSuffix:{g_RootGameObject.name}");
+                        g_RootGameObject.name = prefixText + g_RootGameObject.name + suffixText;
+                    }
+                    ChangeAllUnderObjectName(g_RootGameObject);
+                }
+            }
+        }
+    }
+}
+
---
+++ Assets/meronmksTools/NewInspectorWindow/Editor/NewInspectorWindow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.Reflection;
+
+public class NewInspectorWindow
+{
+
+	[MenuItem("GameObject/NewInspectorWindow %l", false, 30)]
+	private static void ShowGoInspector()
+	{
+		ShowInspector();
+	}
+	
+	[MenuItem("Assets/NewInspectorWindow %l", false, 30)]
+	private static void ShowPjInspector()
+	{
+		ShowInspector();
+	}
+
+	private static void ShowInspector()
+	{
+		var inspectorType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+		var inspectorInstance = ScriptableObject.CreateInstance(inspectorType) as EditorWindow;
+		inspectorInstance.ShowUtility();
+
+		var isLocked = inspectorType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.Public);
+		isLocked.GetSetMethod().Invoke(inspectorInstance, new object[] { true });
+	}
+}

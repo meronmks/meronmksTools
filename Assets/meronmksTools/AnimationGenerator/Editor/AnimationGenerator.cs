6a05e3d8f6360075d4b32ad4138383734ebca0f4
meronmks <meronmks@users.noreply.github.com>
2023-01-07 23:14:48 +0900

    使い勝手向上

--- Assets/meronmksTools/AnimationGenerator/Editor/AnimationGenerator.cs
+++ Assets/meronmksTools/AnimationGenerator/Editor/AnimationGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,17 +11,19 @@ namespace MeronmksTools
     public class AnimationGenerator : EditorWindow
     {
         [SerializeField] private Transform[] g_RootTransform;
-        private string g_CreateAnimPath = "Assets/";
+        private string g_CreateAnimPath = "Animations";
+        private string g_CreateAnimName = "generateAnim";
         private bool isObjActive = true;
         private bool isTransFormPosEx = true;
         private bool isTransFormRotEx = true;
         private bool isTransFormScaEx = true;
         private bool isIgoneVrc_Shape = true;
+        private bool isIgoneWeightZeroShape = false;
         
         [MenuItem("Tools/AnimationGenerator")]
         private static void ShowWindow()
         {
-            GetWindow<AnimationGenerator>(true, "AnimationGenerator", true);
+            GetWindow<AnimationGenerator>(false, "AnimationGenerator", true);
         }
 
         private void OnGUI()
@@ -28,42 +32,62 @@ namespace MeronmksTools
             
             SerializedObject so = new SerializedObject(this);
             SerializedProperty sp_g_RootGameObject = so.FindProperty(nameof(g_RootTransform));
-            EditorGUILayout.PropertyField(sp_g_RootGameObject, new GUIContent("RootGameObject"), true);
-            g_CreateAnimPath = EditorGUILayout.TextField("書き出し先のPath", g_CreateAnimPath);
+            EditorGUILayout.PropertyField(sp_g_RootGameObject, new GUIContent("ターゲットのGameObject"), true);
+            if (g_RootTransform == null || g_RootTransform.Length == 0)
+            {
+                EditorGUILayout.HelpBox("ターゲットのGameObjectが未設定です", MessageType.Error);
+                isDisabled = true;
+            }
+            
+            g_CreateAnimName = EditorGUILayout.TextField("書き出すファイル名", g_CreateAnimName);
+            if (string.IsNullOrEmpty(g_CreateAnimName))
+            {
+                EditorGUILayout.HelpBox("ファイル名が未設定です", MessageType.Error);
+                isDisabled = true;
+            }
+            
+            g_CreateAnimPath = EditorGUILayout.TextField("書き出し先のフォルダ", g_CreateAnimPath);
+            if (new Regex(".*/$").IsMatch(g_CreateAnimPath))
+            {
+                g_CreateAnimPath = g_CreateAnimPath.Remove(g_CreateAnimPath.Length - 1);
+            }
+            
             isObjActive = EditorGUILayout.Toggle("Active状態を書き出す", isObjActive);
             isTransFormPosEx = EditorGUILayout.Toggle("Positionも書き出す", isTransFormPosEx);
             isTransFormRotEx = EditorGUILayout.Toggle("Rotationも書き出す", isTransFormRotEx);
             isTransFormScaEx = EditorGUILayout.Toggle("Scaleも書き出す", isTransFormScaEx);
             isIgoneVrc_Shape = EditorGUILayout.Toggle("vrc.vから始まるShapeを除外", isIgoneVrc_Shape);
+            isIgoneWeightZeroShape = EditorGUILayout.Toggle("Weighが0のShapeを除外", isIgoneWeightZeroShape);
             
-            if (g_RootTransform == null || g_RootTransform.Length == 0)
-            {
-                EditorGUILayout.HelpBox("RootGameObjectが未設定です", MessageType.Error);
-                isDisabled = true;
-            }
-
             using (new EditorGUI.DisabledScope(isDisabled))
             {
                 if (GUILayout.Button(new GUIContent("Generate!"), EditorStyles.miniButton))
                 {
+                    var targetPath = $"Assets/{g_CreateAnimPath}";
                     var animation = new AnimationClip();
 
                     for (int i = 0; i < g_RootTransform.Length; i++)
                     {
                         SkinnedMeshRenderer smr = g_RootTransform[i].GetComponent<SkinnedMeshRenderer>();
                         
-                        //
                         if (smr != null)
                         {
                             var mesh = smr.sharedMesh;
                             for (var j = 0; j < mesh.blendShapeCount; j++)
                             {
+                                // vrc.vから始まるShapeを除外するか
                                 if (isIgoneVrc_Shape && mesh.GetBlendShapeName(j).StartsWith("vrc.v", StringComparison.InvariantCultureIgnoreCase))
                                 {
                                     continue;;
                                 }
+                                
+                                // Weighが0のShapeを除外するか
+                                if (isIgoneWeightZeroShape && smr.GetBlendShapeWeight(j) == 0f)
+                                {
+                                    continue;
+                                }
                                 // BlendShapeを書き出す
-                                AnimationCurve curve = AnimationCurve.Linear(0.0f, smr.GetBlendShapeWeight(j), 0.0f, smr.GetBlendShapeWeight(j));
+                                AnimationCurve curve = AnimationCurve.Linear(0f, smr.GetBlendShapeWeight(j), 0f, smr.GetBlendShapeWeight(j));
                                 animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(SkinnedMeshRenderer), $"blendShape.{mesh.GetBlendShapeName(j)}", curve);
                             }   
                         }
@@ -72,16 +96,16 @@ namespace MeronmksTools
                         if (isObjActive)
                         {
                             var isActiveF = Convert.ToSingle(g_RootTransform[i].gameObject.activeSelf);
-                            AnimationCurve curve = AnimationCurve.Linear(0.0f, isActiveF, 0.0f, isActiveF);
+                            AnimationCurve curve = AnimationCurve.Linear(0f, isActiveF, 0f, isActiveF);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(GameObject), "m_IsActive", curve);
                         }
                         
                         // TransFormのPositionを書き出す
                         if (isTransFormPosEx)
                         {
-                            AnimationCurve curvePX = AnimationCurve.Linear(0.0f, g_RootTransform[i].localPosition.x, 0.0f, g_RootTransform[i].localPosition.x);
-                            AnimationCurve curvePY = AnimationCurve.Linear(0.0f, g_RootTransform[i].localPosition.y, 0.0f, g_RootTransform[i].localPosition.y);
-                            AnimationCurve curvePZ = AnimationCurve.Linear(0.0f, g_RootTransform[i].localPosition.z, 0.0f, g_RootTransform[i].localPosition.z);
+                            AnimationCurve curvePX = AnimationCurve.Linear(0f, g_RootTransform[i].localPosition.x, 0f, g_RootTransform[i].localPosition.x);
+                            AnimationCurve curvePY = AnimationCurve.Linear(0f, g_RootTransform[i].localPosition.y, 0f, g_RootTransform[i].localPosition.y);
+                            AnimationCurve curvePZ = AnimationCurve.Linear(0f, g_RootTransform[i].localPosition.z, 0f, g_RootTransform[i].localPosition.z);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(Transform), "localPosition.x", curvePX);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(Transform), "localPosition.y", curvePY);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(Transform), "localPosition.z", curvePZ);
@@ -90,9 +114,9 @@ namespace MeronmksTools
                         // TransFormのRotationを書き出す
                         if (isTransFormPosEx)
                         {
-                            AnimationCurve curveRX = AnimationCurve.Linear(0.0f, g_RootTransform[i].localRotation.x, 0.0f, g_RootTransform[i].localRotation.x);
-                            AnimationCurve curveRY = AnimationCurve.Linear(0.0f, g_RootTransform[i].localRotation.y, 0.0f, g_RootTransform[i].localRotation.y);
-                            AnimationCurve curveRZ = AnimationCurve.Linear(0.0f, g_RootTransform[i].localRotation.z, 0.0f, g_RootTransform[i].localRotation.z);
+                            AnimationCurve curveRX = AnimationCurve.Linear(0f, g_RootTransform[i].localRotation.x, 0f, g_RootTransform[i].localRotation.x);
+                            AnimationCurve curveRY = AnimationCurve.Linear(0f, g_RootTransform[i].localRotation.y, 0f, g_RootTransform[i].localRotation.y);
+                            AnimationCurve curveRZ = AnimationCurve.Linear(0f, g_RootTransform[i].localRotation.z, 0f, g_RootTransform[i].localRotation.z);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(Transform), "localRotation.x", curveRX);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(Transform), "localRotation.y", curveRY);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(Transform), "localRotation.z", curveRZ);
@@ -101,9 +125,9 @@ namespace MeronmksTools
                         // TransFormのScaleを書き出す
                         if (isTransFormPosEx)
                         {
-                            AnimationCurve curveCX = AnimationCurve.Linear(0.0f, g_RootTransform[i].localScale.x, 0.0f, g_RootTransform[i].localScale.x);
-                            AnimationCurve curveCY = AnimationCurve.Linear(0.0f, g_RootTransform[i].localScale.y, 0.0f, g_RootTransform[i].localScale.y);
-                            AnimationCurve curveCZ = AnimationCurve.Linear(0.0f, g_RootTransform[i].localScale.z, 0.0f, g_RootTransform[i].localScale.z);
+                            AnimationCurve curveCX = AnimationCurve.Linear(0f, g_RootTransform[i].localScale.x, 0f, g_RootTransform[i].localScale.x);
+                            AnimationCurve curveCY = AnimationCurve.Linear(0f, g_RootTransform[i].localScale.y, 0f, g_RootTransform[i].localScale.y);
+                            AnimationCurve curveCZ = AnimationCurve.Linear(0f, g_RootTransform[i].localScale.z, 0f, g_RootTransform[i].localScale.z);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(Transform), "localScale.x", curveCX);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(Transform), "localScale.y", curveCY);
                             animation.SetCurve(GetFullPath(g_RootTransform[i]), typeof(Transform), "localScale.z", curveCZ);
@@ -111,8 +135,26 @@ namespace MeronmksTools
   
                     }
 
-                    AssetDatabase.CreateAsset(animation, $"{g_CreateAnimPath}generateAnim.anim");
-                    Debug.Log($"{g_CreateAnimPath}generateAnim.animに書き出ししました");
+                    // 書き出す前に書き出し先のフォルダ存在確認
+                    if (!AssetDatabase.IsValidFolder(targetPath))
+                    {
+                        // 無ければ[/]で分解して作る
+                        var splitString = targetPath.Split('/');
+                        var combinePath = splitString[0];
+                        foreach (var dir in splitString.Skip(1))
+                        {
+                            // 作る前にフォルダの存在確認
+                            if (!AssetDatabase.IsValidFolder($"{combinePath}/{dir}"))
+                            {
+                                AssetDatabase.CreateFolder(combinePath, dir);
+                            }
+                            combinePath += $"/{dir}";
+                        }
+                    }
+                    
+                    AssetDatabase.CreateAsset(animation, $"{targetPath}/{g_CreateAnimName}.anim");
+                    AssetDatabase.Refresh();
+                    Debug.Log($"{targetPath}/{g_CreateAnimName}.animに書き出ししました");
                 }
             }
             
